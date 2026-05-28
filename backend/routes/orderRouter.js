0cd25c805404f30af74f4018936170b23979ad07
Chandan0Singh <code.chandansingh@gmail.com>
2026-05-28 16:04:07 +0530

    filter api backend

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllOrders, createOrder } = require("../controllers/orderController");
+const { getAllOrders, filterOrders, createOrder } = require("../controllers/orderController");
 
-router.post("/create", createOrder)
+router.post("/create", createOrder);
 
-router.get("/all", getAllOrders)
+router.get("/all", getAllOrders);
+
+router.get("/filter", filterOrders);
 
 module.exports = router;
